4bed748d234ddb85c677296b3d861ddfe8ac0d8d
lafod <lanre.afod@globalaccelerex.com>
2020-07-23 22:19:58 +0100

    updated swagger file

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('api/scf/doc', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
