25eb343d4b42e425f6faa2e55ef94442ac40fed7
lafod <lanre.afod@globalaccelerex.com>
2020-09-28 23:49:02 +0100

    ensure apr is decimal

---
+++ src/migration/1600758470731-added-apr-to-organization.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addedAprToOrganization1600758470731 implements MigrationInterface {
+    name = 'addedAprToOrganization1600758470730'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(" ALTER TABLE `organization` CHANGE COLUMN `apr` `apr` DECIMAL(5,2) NULL DEFAULT NULL");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `organization` DROP COLUMN `apr`");
+    }
+
+}
